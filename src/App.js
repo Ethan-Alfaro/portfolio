4d266d020aa9dde97b4a5f19f164a1d7756282e2
Ethan-Alfaro <80260745+Ethan-Alfaro@users.noreply.github.com>
2021-09-14 19:02:28 +0200

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./App.css";
+import Header from "./components/Header";
+import Skills from "./components/Skills";
+import Projects from "./components/Projects";
+import Contact from "./components/Contact";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Skills />
+      <Projects />
+      <Contact />
+    </div>
+  );
+}
+
+export default App;
